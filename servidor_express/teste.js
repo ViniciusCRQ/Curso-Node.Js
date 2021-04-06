85d379ecdff8472eab1788b624e552ea712f7d55
Vinícius Carvalho Ribeiro Quirino <cabohdmiusb@hotmail.com>
2021-04-05 22:24:42 -0300

    colocando informações na tabela

--- servidor_express/teste.js
+++ servidor_express/teste.js
@@ -1,3 +1,5 @@
+// iniciar o sequelize
+
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize('teste', 'root', 'Crqmysql1678', {
 
@@ -6,10 +8,49 @@ const sequelize = new Sequelize('teste', 'root', 'Crqmysql1678', {
 
 })
 
-sequelize.authenticate().then(function(){
-    console.log('Conectador com sucesso!')
-}).catch(function(erro){
-    console.log('Falha ao se conectar:' + erro)
+//Criando uma tabela
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+Postagem.create({
+    titulo: "titulo comum",
+    conteudo: "asafffafafsf"
+    
+})
+
+const Usuario = sequelize.define('usuarios', {
+
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+
+})
+
+Usuario.create({
+    nome: "Vinicius",
+    sobrenome: "Carvalho",
+    idade: 16,
+    email: "andre"
 })
 
 
+
+
+//sPostagem.sync({force: true})
+//Usuario.sync({force: true})
